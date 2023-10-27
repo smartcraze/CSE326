8fb99cf6c52407a9f82be9d6be4eb21d028d7f28
smartcraze <smartcraze354@gmail.com]>
2023-10-27 09:43:49 +0530

    added home page and mofified login page

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-hey
+// JavaScript to trigger the animation
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector("form");
+    form.classList.add("slide-in");
+});
